a209cd6945e9723419309faad3ea9e148743824b
leomols97 <leomols@hotmail.com>
2024-01-19 11:34:41 +0100

    first commit

---
+++ FlirtGPT/ios/Pods/Target Support Files/Pods-FlirtGPT-FlirtGPTTests/Pods-FlirtGPT-FlirtGPTTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlirtGPT_FlirtGPTTests : NSObject
+@end
+@implementation PodsDummy_Pods_FlirtGPT_FlirtGPTTests
+@end
---
+++ FlirtGPT/ios/Pods/Target Support Files/Pods-FlirtGPT/Pods-FlirtGPT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlirtGPT : NSObject
+@end
+@implementation PodsDummy_Pods_FlirtGPT
+@end
